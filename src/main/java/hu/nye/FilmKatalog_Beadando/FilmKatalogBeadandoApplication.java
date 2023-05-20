338567ce27c201fcaae6411ff5e3d53b4b06a363
Berecz Zsolt <bereczzsolt27@gmail.com>
2023-05-21 00:05:35 +0200

    Initial commit

---
+++ src/main/java/hu/nye/FilmKatalog_Beadando/FilmKatalogBeadandoApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.FilmKatalog_Beadando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmKatalogBeadandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmKatalogBeadandoApplication.class, args);
+	}
+
+}
